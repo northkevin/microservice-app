253c24beb18350a131c28be8d21206ffad502316
Kevin <dev@northkevin.com>
2019-03-09 14:40:36 -0700

    added dockerfile / docker-compose files

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,13 +1,23 @@
 # services/users/project/__init__.py
 
+
+import os
+import sys
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 # set config, using config.py
 app.config.from_object('project.config.DevelopmentConfig')
+
+# for debugging purposes
+# print(app.config, file=sys.stderr)
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
