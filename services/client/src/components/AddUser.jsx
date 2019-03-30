a372bb3f8de1aa2e6087f037c2831c0252c9caf5
Kevin <dev@northkevin.com>
2019-03-30 13:35:27 -0700

    added AJAX request to post data entered in form to my users endpoint

--- services/client/src/components/AddUser.jsx
+++ services/client/src/components/AddUser.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddUser = (props) => {
   return (
-    <form>
+    <form onSubmit={ (event) => props.addUser(event)}>
       <div className="field">
         <input
           name="username"
@@ -10,6 +10,9 @@ const AddUser = (props) => {
           type="text"
           placeholder="Enter a username"
           required
+          value={props.username}
+          onChange={props.handleChange}
+          arbitraryproperty="somthingDumb"
         />
       </div>
       <div className="field">
@@ -19,6 +22,8 @@ const AddUser = (props) => {
           type="email"
           placeholder="Enter an email address"
           required
+          value={props.email}
+          onChange={props.handleChange}
         />
       </div>
       <input
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -10,8 +10,13 @@ class App extends Component
   {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     };
+
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   // 'componentDidMount' will make the method call when Ajax is completed mounting the app
@@ -19,6 +24,46 @@ class App extends Component
   componentDidMount()
   {
     this.getUsers();
+  }
+
+  // Send request to my services/users endpoint .. POST
+  addUser(event)
+  {
+    event.preventDefault();
+    console.log('this.state.username ', this.state.username);
+    console.log('this.state.email ', this.state.email);
+    console.log('sanity check!');
+    console.log('this.state: ', this.state)
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => { console.log(res); })
+      .catch((err) => { console.log(err); });
+  };
+
+  //TODO identify where the 'setState' method is defined
+  //TODO_LESSONS_LEARNED_1 identify where the '.name' property comes from within the 'event.target.name'
+  handleChange(event)
+  {
+    console.log('index.js - App#handleChange(event) - event: ', event);
+    console.log('index.js - App#handleChange(event) - event.type: ', event.type);
+    console.log('index.js - App#handleChange(event) - event.name: ' + event.name);
+    console.log('index.js - App#handleChange(event) - event.toString(): ' + event.toString());
+    console.log('-------------------------------------------------------------------------------------')
+    console.log('index.js - App#handleChange(event) - event.target: ', event.target);
+    console.log('index.js - App#handleChange(event) - event.target.name: ', event.target.name);
+    console.log('index.js - App#handleChange(event) - event.target.class: ', event.target.class);
+    console.log('index.js - App#handleChange(event) - event.target.type: ', event.target.type);
+    console.log('index.js - App#handleChange(event) - event.target.placeholder: ', event.target.placeholder);
+    console.log('index.js - App#handleChange(event) - event.target.required: ', event.target.required);
+    console.log('index.js - App#handleChange(event) - event.target.arbitraryproperty: ', event.target.arbitraryproperty);
+    console.log("index.js - App#handleChange(event) - event.target.getAttribute('arbitraryproperty': ", event.target.getAttribute('arbitraryproperty'));
+    console.log('index.js - App#handleChange(event) - event.target.value: ', event.target.value);
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
   }
 
   render()
@@ -31,7 +76,12 @@ class App extends Component
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
-              <AddUser/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <br/><br/>
               <UsersList users={this.state.users}/>
             </div>
