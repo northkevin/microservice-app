ac764a0263e66880cabeec0a2c5f63f781c5f800
Kevin <dev@northkevin.com>
2019-04-13 18:09:07 -0700

    added 'active' and 'admin' to userlist view and userstatus view
    updated tests

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
 import About from './components/About';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
@@ -170,18 +169,7 @@ class App extends Component
                     />
                   )} />
                   <Route exact path='/' render={() => (
-                    <div>
-                      <h1 className="title is-1">All Users</h1>
-                      <hr/><br/>
-                      <AddUser
-                        username={this.state.username}
-                        email={this.state.email}
-                        addUser={this.addUser}
-                        handleChange={this.handleChange}
-                      />
-                      <br/><br/>
                       <UsersList users={this.state.users}/>
-                    </div>
                   )} />
                   <Route exact path='/about' component={About}/>
                 </Switch>
