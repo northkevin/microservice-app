66adc0c8e7ff3e95eef250bb1e38047bc99678e1
Kevin <dev@northkevin.com>
2019-04-13 17:56:56 -0700

    added admin column to user table
    added admin user to tests requiring authentication

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -238,6 +238,7 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(data['data']['username'] == 'test')
             self.assertTrue(data['data']['email'] == 'test@test.com')
             self.assertTrue(data['data']['active'] is True)
+            self.assertTrue(data['data']['admin'] is False)
             self.assertEqual(response.status_code, 200)
 
     def test_invalid_status(self):
