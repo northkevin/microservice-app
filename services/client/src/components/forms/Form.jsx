092beb13752a630e6333cf5c840c2f7908a309db
Kevin <dev@northkevin.com>
2019-04-27 11:53:31 -0700

    idk

--- services/client/src/components/forms/Form.jsx
+++ services/client/src/components/forms/Form.jsx
@@ -93,10 +93,10 @@ class Form extends Component {
   };
 
   validateEmail(email) {
-    // eslint-disable-next-line
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  };
+  // eslint-disable-next-line
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
 
   allTrue() {
     let formRules = loginFormRules;
