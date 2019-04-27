09ef5a9a9b4f6615058f3843c236318c62422b98
Kevin <dev@northkevin.com>
2019-04-27 10:38:49 -0700

    added validateForm test to unit tests

--- services/client/src/components/__tests__/Form.test.jsx
+++ services/client/src/components/__tests__/Form.test.jsx
@@ -48,6 +48,7 @@ describe('When not authenticated', () => {
     it(`${el.formType} Form submits the form properly`, () => {
       const wrapper = shallow(component);
       wrapper.instance().handleUserFormSubmit = jest.fn();
+      wrapper.instance().validateForm = jest.fn();
       wrapper.update();
       const input = wrapper.find('input[type="email"]');
       expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledTimes(0);
@@ -56,6 +57,7 @@ describe('When not authenticated', () => {
       wrapper.find('form').simulate('submit', el.formData)
       expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledWith(el.formData);
       expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledTimes(1);
+      expect(wrapper.instance().validateForm).toHaveBeenCalledTimes(1);
     });
 
     it(`${el.formType} Form renders a snapshot properly`, () => {
@@ -68,7 +70,7 @@ describe('When not authenticated', () => {
       const input = wrapper.find('input[type="submit"]');
       expect(input.get(0).props.disabled).toEqual(true);
     });
-    
+
   })
 });
 
