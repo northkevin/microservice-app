3901febb74eb6d62aa60820bf897883b4036dc0b
Kevin <dev@northkevin.com>
2019-05-26 16:43:14 -0700

    more debug print statements

--- docker-deploy-stage.sh
+++ docker-deploy-stage.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "docker-deploy-stage bash script is now executing"
+echo "docker-deploy-stage 'pwd' is .. $(pwd)"
+echo "docker-deploy-stage 'ls -al' is .. $(ls -al)"
+
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
 
@@ -18,10 +22,9 @@ then
     register_definition() {
       if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" | $JQ '.taskDefinition.taskDefinitionArn'); then
         echo "Revision: $revision"
-        return 0
       else
         echo "Failed to register task definition"
-        return 1
+        return
       fi
     }
 
@@ -29,9 +32,11 @@ then
     update_service() {
       if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
-        return 1
-      else
-        return 0
+        echo "service: $service .. cluster: $cluster .. revision:$revision"
+        return
+      else  
+        echo "Success updating service."
+        echo "service: $service .. cluster: $cluster .. revision:$revision"
       fi
       
     }
@@ -42,73 +47,68 @@ then
       cluster="test-driven-staging-cluster"
 
       # users
-      # new
       service="testdriven-users-stage-service"
       template="ecs_users_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_ACCOUNT_ID)
       echo "$task_def"
 
-      if [[ register_definition ]]; then 
-        echo "register_definition was successfull for template: $template"
-      else 
-        echo "register_definition failed for template: $template"
+      register_definition
+      status=$?
+      if ! $(exit $status); then
+        echo "register_definition failed."
+        echo "dumping current value of template: $template"
       fi
 
-      if [[ update_service ]]; then 
-        echo "update_service was successfull for service: $service"
-      else 
-        echo "update_service failed for service: $service"
+      update_service
+      status=$?
+      if ! $(exit $status); then
+        echo "register_definition failed."
+        echo "dumping current value of template: $template"
       fi
       
 
       # client
-      # new
       service="testdriven-client-stage-service"
       template="ecs_client_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
 
-      if [[ register_definition ]]; then 
-        echo "register_definition was successfull for template: $template"
-      else 
-        echo "register_definition failed for template: $template"
+      register_definition
+      status=$?
+      if ! $(exit $status); then
+        echo "register_definition failed."
+        echo "dumping current value of template: $template"
       fi
 
-      if [[ update_service ]]; then 
-        echo "update_service was successfull for service: $service"
-      else 
-        echo "update_service failed for service: $service"
+      update_service
+      status=$?
+      if ! $(exit $status); then
+        echo "register_definition failed."
+        echo "dumping current value of template: $template"
       fi
-
-      # register_definition
-      # # new
-      # update_service
 
       # swagger
-      # new
       service="testdriven-swagger-stage-service"
       template="ecs_swagger_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
 
-      if [[ register_definition ]]; then 
-        echo "register_definition was successfull for template: $template"
-      else 
-        echo "register_definition failed for template: $template"
+      register_definition
+      status=$?
+      if ! $(exit $status); then
+        echo "register_definition failed."
+        echo "dumping current value of template: $template"
       fi
 
-      if [[ update_service ]]; then 
-        echo "update_service was successfull for service: $service"
-      else 
-        echo "update_service failed for service: $service"
+      update_service
+      status=$?
+      if ! $(exit $status); then
+        echo "register_definition failed."
+        echo "dumping current value of template: $template"
       fi
-
-      # register_definition
-      # # new
-      # update_service
 
 
     }
