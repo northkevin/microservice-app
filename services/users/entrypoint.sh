a972057bab685570bce456287a171fb15e735ad5
Kevin <dev@northkevin.com>
2019-05-26 18:59:29 -0700

    fixing more mistakes

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -10,4 +10,5 @@ echo "PostgreSQL started"
 
 # python manage.py recreate_db
 # python manage.py seed_db
-gunicorn -b 0.0.0.0:5000 manage:app
+# gunicorn -b 0.0.0.0:5000 manage:app
+python manage.py run -h 0.0.0.0
